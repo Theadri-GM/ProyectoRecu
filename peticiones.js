f2483dccf1824771e75c75ddb764db0f4ebce71c
Adrian Velasco Carrasco <avelcar1108@g.educaand.es>
2023-01-10 21:38:33 +0100

    busca id mediante fetch en api. Fin clase

--- peticiones.js
+++ peticiones.js
@@ -3,6 +3,22 @@
     Primero, voy a comprobar que funciona y que me devuelve los characters que quiero por consola. Para ello:
 */
 
+const aplicacion = document.querySelector('.container')
+
 fetch('https://rickandmortyapi.com/api/character')
     .then(response => response.json())
-    .then(data => console.log(data));
+    .then(data => {
+        data.results.forEach(personaje => {
+            console.log(personaje.id)
+            const p = document.createElement('p')
+            p.innerHTML = personaje.id
+            aplicacion.appendChild(p)
+            const img = document.createElement('img')
+            img.innerHTML = personaje.image
+            aplicacion.appendChild(img)
+        });
+    })
+
+    fetch('https://rickandmortyapi.com/api/character')
+    .then(response => response.json())
+    .then(data => console.log(data))
