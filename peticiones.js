0037cbe82612660bc748d9a0965ede0756071487
Adrian Velasco Carrasco <avelcar1108@g.educaand.es>
2023-01-11 19:13:29 +0100

    Ya mostramos los personajes. API funcionando

--- peticiones.js
+++ peticiones.js
@@ -9,16 +9,36 @@ fetch('https://rickandmortyapi.com/api/character')
     .then(response => response.json())
     .then(data => {
         data.results.forEach(personaje => {
-            console.log(personaje.id)
-            const p = document.createElement('p')
-            p.innerHTML = personaje.id
-            aplicacion.appendChild(p)
-            const img = document.createElement('img')
-            img.innerHTML = personaje.image
-            aplicacion.appendChild(img)
+            crearPersonaje(personaje);
         });
     })
 
-    fetch('https://rickandmortyapi.com/api/character')
-    .then(response => response.json())
-    .then(data => console.log(data))
+
+
+function crearPersonaje(personaje){
+    const contenido = document.createElement('div');
+    contenido.classList.add('personajes');
+
+    const imagenes = document.createElement('div');
+    imagenes.classList.add('imagenes');
+
+    const imagen = document.createElement('img');
+    imagen.src = personaje.image
+
+    const id = document.createElement('p');
+    id.textContent =`#${personaje.id.toString()}`;
+
+    const nombre = document.createElement('p');
+    nombre.classList.add('nombre');
+    nombre.textContent = personaje.name;
+
+    contenido.appendChild(imagen);
+    contenido.appendChild(id);
+    contenido.appendChild(nombre);
+
+    aplicacion.appendChild(contenido);
+}
+
+function buscarPersonajes(event){
+    
+}
